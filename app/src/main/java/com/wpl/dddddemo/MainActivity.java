a1016f928b0235a3dd37a4439f68509ee29d33e1
boyapeng <18211034156@163.com>
2016-11-14 09:52:33 +0800

    Initial commit

---
+++ app/src/main/java/com/wpl/dddddemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.wpl.dddddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        ///  我是王鹏龙
+        //我是卜亚鹏
+    }
+}
